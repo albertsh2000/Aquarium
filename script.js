960f9bda5c2ae37ec4cbf4eb578fe50a67b0b5db
albertsh2000 <albertshahbazyan2000@gmail.com>
2025-06-07 15:14:20 +0400

    added game images and functionality

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("test");
+const fish = document.querySelector(".fish");
+let positionX = 0;
+
+fish.addEventListener("click", (e) => {
+  e.stopPropagation();
+  positionX += 200;
+  fish.style.transform = `translateX(${positionX}px)`;
+});
